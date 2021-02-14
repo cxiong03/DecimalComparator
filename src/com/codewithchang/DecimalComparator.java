cb237eb9aa5e11ca952ceded0ad8d73a46204f70
Chang <cnxiong03@gmail.com>
2021-02-13 19:32:23 -0500

    algorithm wokrs

--- src/com/codewithchang/DecimalComparator.java
+++ src/com/codewithchang/DecimalComparator.java
@@ -5,15 +5,15 @@ public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
 
         // casting - double into int
-        int num1 = (int) (number1 * 100);
-        int num2 = (int) (number2 * 100);
+        int num1 = (int) (number1 * 1000);
+        int num2 = (int) (number2 * 1000);
 
 
         // compare our variables
         if(num1 == num2) {
             System.out.println("True");
             return true;
-        } else if ()
+        }
         System.out.println("False");
         return false;
     }
